5e480100cebf5e821ae59f8647a93c1c0ff5f24f
Amro Shekh Zain <amroshehzen@hotmail.com>
2024-07-06 17:07:45 +0300

    Setup profile layout.

--- lib/layouts/cubit/cubit.dart
+++ lib/layouts/cubit/cubit.dart
@@ -1,6 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,30 +6,23 @@ import 'package:social_app/layouts/cubit/states.dart';
 import 'package:social_app/models/login_model.dart';
 import 'package:social_app/modules/chats/chats_screen.dart';
 import 'package:social_app/modules/home/home_screen.dart';
-
-// import 'package:shop_app/models/login/ShopModel.dart';
-// import 'package:shop_app/modules/login/states.dart';
-// import 'package:shop_app/shared/network/remote/end_points.dart';
-import 'package:social_app/modules/login/states.dart';
 import 'package:social_app/modules/settings/settings_screen.dart';
 import 'package:social_app/modules/users/users_screen.dart';
-
 import '../../modules/new_post/new_post_screen.dart';
 import '../../shared/components/constants.dart';
 
-// import '../../shared/network/remote/dio_helper.dart';
 
-class SocialCubit extends Cubit<SocialState> {
+class SocialCubit extends Cubit<SocialStates> {
   SocialCubit() : super(SocialInitialState());
 
   static SocialCubit get(context) => BlocProvider.of(context);
-  SocialLoginModel? model;
+  SocialLoginModel? userModel;
 
   void getUserData() {
     emit(SocialLoadingState());
     FirebaseFirestore.instance.collection("users").doc(uId).get().then((value) {
-      model = SocialLoginModel.formJson(value.data()!);
-      emit(SocialSuccessState(model!.uId!));
+      userModel = SocialLoginModel.formJson(value.data()!);
+      emit(SocialSuccessState(userModel!.uId!));
     }).catchError((error) {
       emit(SocialErrorState(error.toString()));
     });
--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -3,14 +3,24 @@ class SocialLoginModel {
    String? email;
    String? phone;
    String? uId;
+   String? bio;
+   String? image;
+   String? cover;
    bool? isEmailVerification;
 
-  SocialLoginModel(this.name, this.email, this.phone, this.uId, this.isEmailVerification);
+  SocialLoginModel(this.name, this.email, this.phone, this.uId,
+      this.bio,
+      this.image,
+      this.cover,
+      this.isEmailVerification);
   SocialLoginModel.formJson(Map<String, dynamic> json){
     name = json['name'];
     email = json['email'];
     phone = json['phone'];
     uId = json['uId'];
+    bio = json['bio'];
+    image = json['image'];
+    cover = json['cover'];
     isEmailVerification = json['isEmailVerification'];
   }
 
@@ -20,6 +30,9 @@ class SocialLoginModel {
       "email": email,
       "phone": phone,
       "uId": uId,
+      "bio": bio,
+      "image": image,
+      "cover": cover,
       "isEmailVerification": isEmailVerification,
     };
   }
