dd9c465a646c4a72f3b4d96e0ca3443512f50695
Amro Shekh Zain <amroshehzen@hotmail.com>
2024-07-14 21:19:03 +0300

    Add firebase messaging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/layouts/cubit/cubit.dart';
 import 'package:social_app/layouts/cubit/states.dart';
 import 'package:social_app/modules/login/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:social_app/shared/components/components.dart';
 import 'package:social_app/shared/components/constants.dart';
 import 'package:social_app/shared/cubit_observer.dart';
 import 'package:social_app/shared/shared_preferences.dart';
 import 'package:social_app/shared/styles/themes.dart';
 import 'firebase_options.dart';
 import 'layouts/social_layout.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async
+{
+  print('on background message');
+  print(message.data.toString());
+
+  await showToast(message: 'on background message', state: ToastStates.SUCCESS,);
+}
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,6 +37,33 @@ Future<void> main() async {
   } else {
     startWidget = SocialLayout();
   }
+  FirebaseMessaging.instance.requestPermission(
+      alert: true, badge: true, provisional: true, sound: true);
+
+  var token = await FirebaseMessaging.instance.getToken();
+
+  print(token);
+  // foreground fcm
+  FirebaseMessaging.onMessage.listen((event)
+  {
+    print('on message');
+    print(event.data.toString());
+
+    showToast(message: 'on message', state: ToastStates.SUCCESS,);
+  });
+
+  // when click on notification to open app
+  FirebaseMessaging.onMessageOpenedApp.listen((event)
+  {
+    print('on message opened app');
+    print(event.data.toString());
+
+    showToast(message: 'on message opened app', state: ToastStates.SUCCESS,);
+  });
+
+  // background fcm
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(MyApp(startWidget));
 }
 
